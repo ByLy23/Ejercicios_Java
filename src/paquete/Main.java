69c14352a94be11044a37ec97e6621cc3e383ff6
ByLy23 <39969132+ByLy23@users.noreply.github.com>
2018-08-14 19:13:30 -0600

    Inicializacion de la tarea

---
+++ src/paquete/Main.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paquete;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author byron
+ */
+public class Main {
+    public static void main(String[] args){
+       int lector=0;
+       do{
+       System.out.println("--[IPC1]Tarea3_201700733--");
+       System.out.println("1.Usuarios\n2.Contar digitos repetidos\n3.3 numeros de mayor a menor\n4.SCalcular promedio\n5.Salir");
+     try{
+       Scanner sc= new Scanner(System.in);
+       lector= sc.nextInt();
+     }catch(InputMismatchException exce){
+         System.out.println("no letras");
+     }
+       switch(lector){
+           case 1:
+               
+               break;
+           case 2:
+             
+               break;
+               case 3:  
+                  
+               break;
+           case 4:
+           
+               break;
+               case 5:
+                   System.out.println("gracias por visitarnos espero vuelvas pronto :v");
+                   break;
+               default:
+                    System.out.println("No es Correcto");
+                   break;
+       }
+       }while(lector!=5);
+   }
+}
